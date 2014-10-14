4bf5e3908f4e78addeb5619b7118e34b08327854
Benoit Gantaume <benoit@agilidee.com>
2014-10-14 10:00:46 +0200

    Added one_line method to addresses

--- app/models/dorsale/address.rb
+++ app/models/dorsale/address.rb
@@ -2,5 +2,9 @@ module Dorsale
   class Address < ActiveRecord::Base
     validates_presence_of :city
     belongs_to :addressable, polymorphic: true
+
+    def one_line
+      result = [street, street_bis, [zip, city].join(' '), country].join(', ')
+    end
   end
 end
--- spec/models/dorsale/address_spec.rb
+++ spec/models/dorsale/address_spec.rb
@@ -4,5 +4,16 @@ module Dorsale
   RSpec.describe Address, :type => :model do
     it {should validate_presence_of :city}
     it {should belong_to :addressable}
+    describe '#one_line' do
+      it 'should build a one line address'do
+        address = Address.create(street: '3 Rue Marx Dormoy', 
+                                 street_bis: 'L\'atelier',
+                                 zip: '13004',
+                                 city: 'Marseille', 
+                                 country: 'France')
+        expect(address.one_line).to eq('3 Rue Marx Dormoy, L\'atelier, 13004 Marseille, France')
+
+      end
+    end
   end
 end
