9046164fa2c0fe041d03855271af5ca7baf62aa5
Benoit Gantaume <benoit@agilidee.com>
2014-10-14 10:53:22 +0200

    tuning one_line method

--- app/models/dorsale/address.rb
+++ app/models/dorsale/address.rb
@@ -4,7 +4,7 @@ module Dorsale
     belongs_to :addressable, polymorphic: true
 
     def one_line
-      result = [street, street_bis, [zip, city].join(' '), country].join(', ')
+      result = [street, street_bis, [zip, city].delete_if {|e| e.blank?}.join(' '), country].delete_if {|e| e.blank?}.join(', ')
     end
   end
 end
--- spec/models/dorsale/address_spec.rb
+++ spec/models/dorsale/address_spec.rb
@@ -5,14 +5,31 @@ module Dorsale
     it {should validate_presence_of :city}
     it {should belong_to :addressable}
     describe '#one_line' do
-      it 'should build a one line address'do
+      it 'should build a one line address' do
         address = Address.create(street: '3 Rue Marx Dormoy', 
                                  street_bis: 'L\'atelier',
                                  zip: '13004',
                                  city: 'Marseille', 
                                  country: 'France')
         expect(address.one_line).to eq('3 Rue Marx Dormoy, L\'atelier, 13004 Marseille, France')
+      end
 
+      it 'should remove useless commas' do
+        address = Address.create(street: '3 Rue Marx Dormoy', 
+                                 street_bis: '',
+                                 zip: '13004',
+                                 city: 'Marseille', 
+                                 country: '')
+        expect(address.one_line).to eq('3 Rue Marx Dormoy, 13004 Marseille')
+      end
+
+      it 'should remove useless commas' do
+        address = Address.create(street: '3 Rue Marx Dormoy', 
+                                 street_bis: '',
+                                 zip: '',
+                                 city: 'Marseille', 
+                                 country: '')
+        expect(address.one_line).to eq('3 Rue Marx Dormoy, Marseille')
       end
     end
   end
