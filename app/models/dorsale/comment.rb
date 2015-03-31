d2c3d3cd052e18237f0dab0e75a84dd1f1ea5d55
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-03-31 09:05:41 +0200

    Remove unused code

--- app/models/dorsale/comment.rb
+++ app/models/dorsale/comment.rb
@@ -2,7 +2,6 @@ module Dorsale
   class Comment < ActiveRecord::Base
     belongs_to :user,        polymorphic: true
     belongs_to :commentable, polymorphic: true
-    has_many :events, as: :eventable, dependent: :destroy
     
     validates :commentable, presence: true
     validates :text,        presence: true
