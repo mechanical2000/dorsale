87759227f9fbc7eb5138d7a6789123fb7e6a9964
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-04-09 14:08:17 +0200

    Add filters button

--- app/helpers/dorsale/button_helper.rb
+++ app/helpers/dorsale/button_helper.rb
@@ -109,5 +109,29 @@ module Dorsale
       dorsale_button(url, options)
     end
 
+    def filter_submit_button(options = {})
+      options[:class] ||= "btn"
+      options[:type]  ||= "submit"
+
+      text = options.delete(:text) || t("actions.filter")
+      icon = options.delete(:icon) || "filter"
+
+      content_tag(:button, options) do
+        icon(icon) + " " + text
+      end
+    end
+
+    def filter_reset_button(options = {})
+      options[:class] ||= "btn reset"
+      options[:type]  ||= "submit"
+
+      text = options.delete(:text) || t("actions.reset")
+      icon = options.delete(:icon) || "rotate-left"
+
+      content_tag(:button, options) do
+        icon(icon) + " " + text
+      end
+    end
+
   end
 end
