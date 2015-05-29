0fa16892aff00f634cd259f21148348d7e814ec1
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-05-29 12:14:46 +0200

    Add web_link, email_link, tel_link helpers

--- app/helpers/dorsale/link_helper.rb
+++ app/helpers/dorsale/link_helper.rb
@@ -5,5 +5,30 @@ module Dorsale
       link_to(name, options, html_options, &block)
     end
 
+    def web_link(text, opts = {})
+      return if text.to_s.blank?
+
+      href = text
+      href = "http://#{text}" unless text.include?("://")
+
+      link_to(tex, href, opts)
+    end
+
+    def email_link(text, opts = {})
+      return if text.to_s.blank?
+
+      href = "mailto:#{text}"
+
+      link_to(text, href, opts)
+    end
+
+    def tel_link(text, opts = {})
+      return if text.to_s.blank?
+
+      href = "tel:#{text}"
+
+      link_to(text, href, opts)
+    end
+
   end
 end
