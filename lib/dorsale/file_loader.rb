4a3462ba80a84eaf135db6e7638cff685fce4023
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-06-25 16:26:23 +0200

    Change load_dorsale_file to dorsale_file

--- lib/dorsale/file_loader.rb
+++ lib/dorsale/file_loader.rb
@@ -1,11 +1,10 @@
 module Kernel
-  def load_dorsale_file
+  def dorsale_file
     app_file      = caller.first.split(":").first
     app_root      = ::Rails.application.root.to_s
     relative_file = app_file.gsub(app_root, "")
     dorsale_root  = ::Dorsale::Engine.root.to_s
-    dorsale_file  = ::File.join(dorsale_root, relative_file)
 
-    require dorsale_file
+    ::File.join(dorsale_root, relative_file)
   end
 end
