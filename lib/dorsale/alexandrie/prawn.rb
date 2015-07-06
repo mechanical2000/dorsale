315e68c6b9f893553d10103dacd25f01d41472f5
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-07-06 14:48:59 +0200

    Change pdf merge method

---
+++ lib/dorsale/alexandrie/prawn.rb
@@ -0,0 +1,17 @@
+module Dorsale
+  module Alexandrie
+    module Prawn
+      def render_with_attachments
+        final_pdf = ::CombinePDF.parse(self.render)
+
+        @main_document.attachments.each do |attachment|
+          next unless File.extname(attachment.file.path) == ".pdf"
+
+          final_pdf << ::CombinePDF.load(attachment.file.path)
+        end
+
+        final_pdf.to_pdf
+      end
+    end
+  end
+end
