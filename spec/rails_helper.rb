d11ee107561c9b3e36b322ca01859d6309995159
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-07-06 15:25:43 +0200

    Test PDF attachments merge

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'cancan/matchers'
+require "yomu"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require "factory_girl_rails"
