19b71475df269db807a4c2657a42f14c6958fbd2
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-02-08 11:57:19 +0100

    Fix tasks cron

---
+++ app/commands/dorsale/flyboy/task_commands.rb
@@ -0,0 +1,11 @@
+module Dorsale::Flyboy::TaskCommands
+  def self.send_daily_term_emails!
+    ::Dorsale::Flyboy::Task.all.each do |task|
+      next if task.done?
+      next if task.term != Date.today
+      next if task.owner.nil?
+
+      ::Dorsale::Flyboy::TaskMailer.term_email(task).deliver_later
+    end
+  end
+end
