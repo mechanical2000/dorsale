346d78073b56d0afec647a1b1632b1de5fa321be
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-06-17 11:14:27 +0200

    Small refactor on Expense Gun

--- app/controllers/dorsale/expense_gun/application_controller.rb
+++ app/controllers/dorsale/expense_gun/application_controller.rb
@@ -1,7 +1,11 @@
 module Dorsale
   module ExpenseGun
     class ApplicationController < ::Dorsale::ApplicationController
+      before_action :set_form_variables
 
+      def set_form_variables
+        @categories ||= ::Dorsale::ExpenseGun::Category.all
+      end
     end
   end
 end
