dea3fb823488c1600c876cba7511befb9f6f7532
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-09-01 17:19:23 +0200

    Fix expensegun vat deductible form

--- app/models/dorsale/expense_gun/category.rb
+++ app/models/dorsale/expense_gun/category.rb
@@ -2,7 +2,9 @@ module Dorsale
   module ExpenseGun
     class Category < ActiveRecord::Base
       self.table_name = "dorsale_expense_gun_categories"
+
       validates :name, presence: true
+      validates :vat_deductible, inclusion: {in: [true, false]}
     end
   end
 end
