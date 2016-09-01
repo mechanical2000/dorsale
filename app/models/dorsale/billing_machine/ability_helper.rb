b5e439a04943f17b669c4978e17fb6e9dd1a9d76
Karb0 <carbonel.mathieu@gmail.com>
2016-09-01 17:24:21 +0200

    change abilities

--- app/models/dorsale/billing_machine/ability_helper.rb
+++ app/models/dorsale/billing_machine/ability_helper.rb
@@ -4,13 +4,7 @@ module Dorsale
       def define_dorsale_billing_machine_abilities
         can [:list, :create, :read, :update, :pay, :copy, :download, :email], ::Dorsale::BillingMachine::Invoice
         can [:list, :create, :read, :update, :delete, :copy, :download], ::Dorsale::BillingMachine::Quotation
-      end
-
-      def define_dorsale_billing_machine_id_cards
         can [:index, :create, :update], Dorsale::BillingMachine::IdCard
-      end
-
-      def define_dorsale_billing_machine_payment_terms
         can [:index, :create, :update], Dorsale::BillingMachine::PaymentTerm
       end
     end
--- app/models/dorsale/expense_gun/ability_helper.rb
+++ app/models/dorsale/expense_gun/ability_helper.rb
@@ -3,12 +3,8 @@ module Dorsale
     module AbilityHelper
       def define_dorsale_expense_gun_abilities
         can [:list, :create, :show, :edit, :submit , :accept, :refuse, :cancel], ::Dorsale::ExpenseGun::Expense
+        can [:index, :create, :update], ::Dorsale::ExpenseGun::Category
       end
-
-
-    def define_dorsale_expense_gun_categories
-      can [:index, :create, :update], ::Dorsale::ExpenseGun::Category
-    end
     end
   end
 end
