dce4214bfa38ebc960f56ab373ed81436fe056af
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-09-11 11:15:46 +0200

    Fix guid for STI

--- app/models/dorsale/billing_machine/small_data/filter_strategy_by_customer.rb
+++ app/models/dorsale/billing_machine/small_data/filter_strategy_by_customer.rb
@@ -1,8 +1,6 @@
 class Dorsale::BillingMachine::SmallData::FilterStrategyByCustomer < ::Dorsale::SmallData::FilterStrategy
   def apply(query, value)
     type, id = value.split("-")
-    type = "Dorsale::CustomerVault::Person" if type == "Dorsale::CustomerVault::Corporation"
-    type = "Dorsale::CustomerVault::Person" if type == "Dorsale::CustomerVault::Individual"
     query.where(customer_type: type, customer_id: id)
   end
 end
