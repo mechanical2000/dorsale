bbcd614d8ed2c079c84182473295e652ad2e5aed
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-11-17 09:11:52 +0100

    Refactor sortable uuid generator

--- app/libs/dorsale/sortable_uuid_generator.rb
+++ app/libs/dorsale/sortable_uuid_generator.rb
@@ -1,11 +1,11 @@
 # Microtime based uuids to be sortable
 class Dorsale::SortableUUIDGenerator
-  REGEX_WITH_DASH    = /^([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})$/
-  REGEX_WITHOUT_DASH = /^([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})$/
+  REGEX_WITH_DASHES    = /^([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})$/
+  REGEX_WITHOUT_DASHES = /^([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})$/
 
   def self.generate
-    prefix = Time.zone.now.to_f.to_s.gsub(".", "").ljust(20, "0").to_i.to_s(16)
-    suffix = SecureRandom.hex(16).slice(0, 32-prefix.length)
-    (prefix + suffix).gsub(REGEX_WITHOUT_DASH, '\1-\2-\3-\4-\5')
+    prefix = Time.zone.now.strftime("%s%9N").to_i.to_s(16)
+    suffix = SecureRandom.hex(8)
+    (prefix + suffix).gsub(REGEX_WITHOUT_DASHES, '\1-\2-\3-\4-\5')
   end
 end
--- spec/libs/dorsale/sortable_uuid_generator_spec.rb
+++ spec/libs/dorsale/sortable_uuid_generator_spec.rb
@@ -3,11 +3,27 @@ require 'rails_helper'
 RSpec.describe Dorsale::SortableUUIDGenerator do
   it "should generate a valid uuid" do
     uuid = described_class.generate
-    expect(uuid).to match described_class::REGEX_WITH_DASH
+    expect(uuid).to match described_class::REGEX_WITH_DASHES
+  end
+
+  it "should still work in 100 years" do
+    Timecop.travel "2116-11-17"
+    uuid = described_class.generate
+    expect(uuid).to match described_class::REGEX_WITH_DASHES
   end
 
   it "should be incremental" do
-    uuids = 1_000.times.map { described_class.generate }
+    uuids = []
+    1_000.times { uuids << described_class.generate }
+    Timecop.travel 1.day.from_now
+    1_000.times { uuids << described_class.generate }
+    Timecop.travel 1.month.from_now
+    1_000.times { uuids << described_class.generate }
+    Timecop.travel 1.year.from_now
+    1_000.times { uuids << described_class.generate }
+    Timecop.travel 10.years.from_now
+    1_000.times { uuids << described_class.generate }
+
     expect(uuids).to eq uuids.dup.sort
   end
 end
