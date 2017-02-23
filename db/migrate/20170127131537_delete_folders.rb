44be548957a45aee213da6fae047771b3eb6005c
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-02-23 09:06:43 +0100

    Uncomment migration line

--- db/migrate/20170127131537_delete_folders.rb
+++ db/migrate/20170127131537_delete_folders.rb
@@ -1,7 +1,7 @@
 class DeleteFolders < ActiveRecord::Migration[5.0]
   def change
     # Comment this migration or only this line to keep or delete folders
-    # raise "Hi ! Do you want to delete folders ?"
+    raise "Hi ! Do you want to delete folders ?"
     drop_table :dorsale_flyboy_folders
     Dorsale::Flyboy::Task
       .where(taskable_type: "Dorsale::Flyboy::Folder")
