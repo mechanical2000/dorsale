c07c315e82a1a7cdd3f80b7b52bf480cd6ca6f9c
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-04-06 17:14:51 +0200

    Fix migration

--- db/migrate/20170406122047_add_attribute_to_dorsale_customer_vault_people.rb
+++ db/migrate/20170406122047_add_attribute_to_dorsale_customer_vault_people.rb
@@ -1,12 +1,6 @@
 class AddAttributeToDorsaleCustomerVaultPeople < ActiveRecord::Migration[5.0]
   class Person < ActiveRecord::Base
-    self.table_name = 'dorsale_customer_vault_people'
-  end
-
-  class Individual < Person
-  end
-
-  class Corporation < Person
+    self.table_name = :dorsale_customer_vault_people
   end
 
   def change
@@ -17,24 +11,26 @@ class AddAttributeToDorsaleCustomerVaultPeople < ActiveRecord::Migration[5.0]
   end
 
   def migrate_individuals_context
-    Individual.all.each do |individual|
-      data = JSON.parse(individual.data)
-      individual.context = data["context"]
+    Dorsale::CustomerVault::Individual.all.each do |individual|
+      data = JSON.parse(individual.data_before_type_cast)
+
+      individual.update_columns context: data["context"]
       data.delete("context")
-      individual.data = data.to_json
-      individual.save!
+
+      Person.where(id: individual.id).update_all(data: data.to_json)
     end
   end
 
   def migrate_corporations_immatriculation_numbers
-    Corporation.all.each do |corporation|
-      data = JSON.parse(corporation.data)
+    Dorsale::CustomerVault::Corporation.all.each do |corporation|
+      data = JSON.parse(corporation.data_before_type_cast)
+
       immatriculation_number = "#{data['immatriculation_number_1']} / #{data['immatriculation_number_2']}"
-      data.delete["immatriculation_number_1"]
-      data.delete["immatriculation_number_2"]
+      data.delete("immatriculation_number_1")
+      data.delete("immatriculation_number_2")
       data["immatriculation_number"] = immatriculation_number
-      corporation.data = data.to_json
-      corporation.save!
+
+      Person.where(id: corporation.id).update_all(data: data.to_json)
     end
   end
 end
