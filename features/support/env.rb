9aa2c968cce7ac7d17e37057d960434ce2f4a01f
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-11-02 17:13:52 +0100

    Temp disable Zonebie in cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,7 @@
 ENV["RAILS_ROOT"] ||= File.expand_path("../../../spec/dummy", __FILE__)
 require "cucumber/rails"
 require "agilibox/cucumber_config"
+def Zonebie.set_random_timezone
+  # https://github.com/agilidee/dorsale/issues/273
+end
 Agilibox::CucumberConfig.require_all_helpers!
